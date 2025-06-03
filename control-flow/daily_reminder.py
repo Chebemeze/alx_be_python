a9b342300993829f88fe285552329bc26af0d79e
Chebemeze <chebemeze@gmail.com>
2025-06-03 05:38:42 +0100

    Changed display message

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,7 +1,7 @@
-task = input("Enter your task for today: ")
-priority = input("what's the priority level (high, medium, low) ").lower()
+task = input("Enter your task: ")
+priority = input("Priority (high, medium, low) ").lower()
 
-time_bound = input("Is this task time bound (yes/no): ")
+time_bound = input("Is it time bound? (yes/no): ").lower()
 
 yesno_list =["yes", "no"]
 
