c63eac02afa06cb18f64701838263278bc702568
Chebemeze <chebemeze@gmail.com>
2025-06-08 19:21:59 +0100

    debugged code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,22 @@
 def perform_operation(num1, num2, operation):
 
-  if operation == "+":
-    return num1 + num2
-  elif operation == "-":
-    return num1 - num2
-  elif operation == "*":
-    return num1 * num2
-  elif operation == "/":
-    if num1 == 0 or num2 == 0:
-      return ("Cannot divide by zero. Kindly provide nonzero values")
-    else:
-      return num1 - num2
+  op_list = ["add", "subtract", "multiply", "divide"]
+ 
+  for i in range(0,4):
+
+    if op_list[i] == operation:
+      if op_list[i] == "add":
+        return num1 + num2
+      elif op_list[i] == "subtract":
+        return num1 - num2
+      elif op_list[i] == "multiply":
+        return num1 * num2
+      elif op_list[i] == "divide":
+          if num1 == 0 or num2 == 0:
+            return ("Cannot divide by zero. Kindly provide nonzero values")
+          else:
+            return num1 - num2 
+
+
+
+
