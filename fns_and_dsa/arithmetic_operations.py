33a0fc7678bd3815eee74f5db042424ba33e0d22
Chebemeze <chebemeze@gmail.com>
2025-06-08 19:25:41 +0100

    Added divide sign

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -15,7 +15,7 @@ def perform_operation(num1, num2, operation):
           if num1 == 0 or num2 == 0:
             return ("Cannot divide by zero. Kindly provide nonzero values")
           else:
-            return num1 - num2 
+            return num1 / num2 
 
 
 
