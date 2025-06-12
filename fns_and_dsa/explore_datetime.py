bf065a56fd433c528fdbe099397aa3bc17a77c4c
Chebemeze <chebemeze@gmail.com>
2025-06-12 04:38:29 +0100

    formatted future date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -10,7 +10,7 @@ def display_current_datetime():
     nonlocal current_date
     number_of_days = int(input("Enter the number of days to add to the current date: "))
     future_date = current_date + timedelta(days = number_of_days)
-    print(future_date) 
+    print(future_date.strftime("%Y-%m-%d"))
   calculate_future_date()
 
 display_current_datetime()
