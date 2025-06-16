71fea138863da9b62a7f4c513afaa27c956a2bc0
Chebemeze <chebemeze@gmail.com>
2025-06-16 20:14:03 +0100

    adjusted code to account for ZerodivisionError and also alll four test cases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,7 +17,7 @@ class TestSimpleCalculator(unittest.TestCase):
     self.assertAlmostEqual(self.calc.add(0.5, -5.0), -4.5)
     # Add more assertions to thoroughly test the add method.
 
-  def test_substraction(self):
+  def test_subtraction(self):
     self.assertEqual(self.calc.subtract(-1, -1), 0)
     self.assertEqual(self.calc.subtract(5, 2), 3)
     self.assertEqual(self.calc.subtract(5, -1), 6)
@@ -28,7 +28,7 @@ class TestSimpleCalculator(unittest.TestCase):
     self.assertAlmostEqual(self.calc.subtract(0.5, -5.0), 5.5)
     self.assertAlmostEqual(self.calc.subtract(-0.5, 5.0), -5.5)
   
-  def multiplication(self):
+  def test_multiply(self):
     self.assertEqual(self.calc.multiply(-1, -1), 1)
     self.assertEqual(self.calc.multiply(4, 10), 40)
     self.assertEqual(self.calc.multiply(100, 100), 10000)
@@ -39,15 +39,17 @@ class TestSimpleCalculator(unittest.TestCase):
     self.assertAlmostEqual(self.calc.multiply(0.5, -5.0), -2.5)
     self.assertAlmostEqual(self.calc.multiply(0.5, 5.0), 2.5)
   
-  def division(self):
+  def test_divide(self):
     self.assertEqual(self.calc.divide(-1, -1), 1)
     self.assertEqual(self.calc.divide(5, 2), 2.5)
     self.assertEqual(self.calc.divide(7, -3), -2.3333333333333335)
     self.assertEqual(self.calc.divide(-20, 2), -10)
     self.assertAlmostEqual(self.calc.divide(10.0, 0.5), 20)
     self.assertAlmostEqual(self.calc.divide(-20.5, 0.5), -41.0)
-    self.assertRaises(ZeroDivisionError, self.cal.divide, 0, 0)
-    self.assertRaises(ZeroDivisionError, self.cal.divide, 60, 0)
+
+    """The above code addresses ZeroDivisionError. the test is successful when None is returned"""
+    self.assertIsNone(self.calc.divide(0, 0))
+    self.assertIsNone(self.calc.divide(60, 0))
 
 # Remember to write additional test methods for subtract, multiply, and divide.
 
